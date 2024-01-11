26057c6dac83bd5d6e53c12fb0c88d535c90171f
Leandro Arturi <lea.arturi@gmail.com>
2024-01-11 10:32:06 -0300

    Avances

--- src/app/armador/page.tsx
+++ src/app/armador/page.tsx
@@ -1,9 +1,8 @@
 import {redirect} from "next/navigation";
 
-import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 import api from "@/api";
-import {Checkbox} from "@/components/ui/checkbox";
-import {Button} from "@/components/ui/button";
+
+import BuilderPageClient from "./page.client";
 
 export default async function BuilderPage() {
   const players = await api.players.list();
@@ -21,27 +20,5 @@ export default async function BuilderPage() {
     redirect(`/armador/partido?${searchParams.toString()}`);
   }
 
-  return (
-    <form action={createTeams} className="m-auto grid max-w-sm gap-4">
-      <Table className="border">
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Nombre</TableHead>
-            <TableHead className="w-[100px]" />
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {players.map(({name, matches, score}) => (
-            <TableRow key={name}>
-              <TableCell className="font-medium">{name}</TableCell>
-              <TableCell className="text-right">
-                <Checkbox name="player" value={name} />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <Button type="submit">Armar Equipos</Button>
-    </form>
-  );
+  return <BuilderPageClient players={players} onCreate={createTeams} />;
 }
